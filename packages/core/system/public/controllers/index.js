b7f29ec4bbfb374cef3ee74be7ea2f68b91ab4cc
Blue <blue@ismashrobots.com>
2015-10-06 00:37:57 -0700

    Messing with phone numbers

--- packages/core/system/public/controllers/index.js
+++ packages/core/system/public/controllers/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module('mean.system', ['cwill747.phonenumber']).controller('IndexController', ['$scope', 'Global',
-  function($scope, Global) {
+var app = angular.module('mean.system');
+
+app.controller('IndexController', ['$scope', '$filter', 'Global',
+  function($scope, $filter, Global) {
     $scope.global = Global;
     $scope.sites = {
       'makeapoint':{
@@ -55,6 +57,52 @@ angular.module('mean.system', ['cwill747.phonenumber']).controller('IndexControl
       }
     });
 
-    $scope.initializedPhoneNumber = '3133536767';
+    $scope.PhoneNumber = $filter("phone")(5557771234);
   }
 ]);
+
+app.filter('phone', function () {
+    return function (phone) {
+
+        if (!phone) { return ''; }
+
+        var value = phone.toString().trim().replace(/^\+/, '');
+
+        if (value.match(/[^0-9]/)) {
+            return phone;
+        }
+
+        var country, city, number;
+
+        switch (value.length) {
+            case 10: // +1PPP####### -> C (PPP) ###-####
+                country = 1;
+                city = value.slice(0, 3);
+                number = value.slice(3);
+                break;
+
+            case 11: // +CPPP####### -> CCC (PP) ###-####
+                country = value[0];
+                city = value.slice(1, 4);
+                number = value.slice(4);
+                break;
+
+            case 12: // +CCCPP####### -> CCC (PP) ###-####
+                country = value.slice(0, 3);
+                city = value.slice(3, 5);
+                number = value.slice(5);
+                break;
+
+            default:
+                return phone;
+        }
+
+        if (country == 1) {
+            country = "";
+        }
+
+        number = number.slice(0, 3) + '-' + number.slice(3);
+
+        return (country + " (" + city + ") " + number).trim();
+    };
+});
