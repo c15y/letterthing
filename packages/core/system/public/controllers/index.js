b1ef79c0d4493ed6a7a07dbaf88fa46106b11640
Blue <blue@ismashrobots.com>
2015-10-05 22:17:30 -0700

    phoneformat

--- packages/core/system/public/controllers/index.js
+++ packages/core/system/public/controllers/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
-  function($scope, Global) {
+var app = angular.module('mean.system');
+
+app.controller('IndexController', ['$scope', '$filter', 'Global',
+  function($scope, $filter, Global) {
     $scope.global = Global;
     $scope.sites = {
       'makeapoint':{
@@ -54,5 +56,52 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
         console.log('currentSlide:', currentSlide);
       }
     });
+
+    $scope.PhoneNumber = $filter("tel")(5419754290);
   }
 ]);
+
+app.filter('tel', function () {
+    return function (tel) {
+        if (!tel) { return ''; }
+
+        var value = tel.toString().trim().replace(/^\+/, '');
+
+        if (value.match(/[^0-9]/)) {
+            return tel;
+        }
+
+        var country, city, number;
+
+        switch (value.length) {
+            case 10: // +1PPP####### -> C (PPP) ###-####
+                country = 1;
+                city = value.slice(0, 3);
+                number = value.slice(3);
+                break;
+
+            case 11: // +CPPP####### -> CCC (PP) ###-####
+                country = value[0];
+                city = value.slice(1, 4);
+                number = value.slice(4);
+                break;
+
+            case 12: // +CCCPP####### -> CCC (PP) ###-####
+                country = value.slice(0, 3);
+                city = value.slice(3, 5);
+                number = value.slice(5);
+                break;
+
+            default:
+                return tel;
+        }
+
+        if (country == 1) {
+            country = "";
+        }
+
+        number = number.slice(0, 3) + '-' + number.slice(3);
+
+        return (country + " (" + city + ") " + number).trim();
+    };
+});
