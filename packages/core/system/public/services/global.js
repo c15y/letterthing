8727ae40d96044a9292f5af23b25d242c4fd2a24
Blue <blue@ismashrobots.com>
2015-10-07 15:06:44 -0700

    auth

--- packages/core/system/public/services/global.js
+++ packages/core/system/public/services/global.js
@@ -8,11 +8,15 @@ angular.module('mean.system').factory('Global', [
     _this._data = {
       user: window.user,
       authenticated: false,
-      isAdmin: false
+      isAdmin: false,
+      isManager: false,
+      isOperator: false
     };
     if (window.user && window.user.roles) {
       _this._data.authenticated = window.user.roles.length;
       _this._data.isAdmin = window.user.roles.indexOf('admin') !== -1;
+      _this._data.isManager = _this._data.isAdmin || window.user.roles.indexOf('manager') !== -1;
+      _this._data.isOperator = _this._data.isManager || window.user.roles.indexOf('operator') !== -1;
     }
     return _this._data;
   }
