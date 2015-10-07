8897aa5864084329b9703479d978516b79045ebf
Blue <blue@ismashrobots.com>
2015-10-07 01:58:21 -0700

    Misc

--- packages/core/system/public/controllers/msc.js
+++ packages/core/system/public/controllers/msc.js
@@ -1,5 +1,7 @@
 'use strict';
 
-angular.module('mean.system').controller('MSCController', function($scope, $stateParams) {
-  $scope.MSC = $stateParams.msc;
-});
+angular.module('mean.system').controller('MSCController', ['$scope', 'Global', '$stateParams',
+  function($scope, Global, $stateParams) {
+    $scope.global = Global;
+    $scope.MSC = $stateParams.msc;
+}]);
