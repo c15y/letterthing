994e206a07bcdd4a805b287adb75d6d2c72337b0
Blue <blue@ismashrobots.com>
2015-10-10 11:13:24 -0700

    FileUploader

--- packages/core/system/public/controllers/letter.js
+++ packages/core/system/public/controllers/letter.js
@@ -1,20 +1,99 @@
 'use strict';
 
-angular.module('mean.system').controller('LetterController', ['$scope', '$modalInstance', 'Letter',
-  function ($scope, $modalInstance, Letter) {
+angular.module('mean.system').controller('LetterController', ['$scope', 'FileUploader',
+  function($scope, FileUploader) {
+    var uploader = $scope.uploader = new FileUploader({
+      url: '/api/images'
+    });
 
-    $scope.images = []
+    uploader.filters.push({
+      name: 'imageFilter',
+      fn: function(item /*{File|FileLikeObject}*/, options) {
+        var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+        return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+      }
+    });
 
-    $scope.submit = function () {
-      Letter.submit($scope.images);
-      $modalInstance.close();
+    uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+      console.info('onWhenAddingFileFailed', item, filter, options);
+    };
+    uploader.onAfterAddingFile = function(fileItem) {
+      console.info('onAfterAddingFile', fileItem);
+    };
+    uploader.onAfterAddingAll = function(addedFileItems) {
+      console.info('onAfterAddingAll', addedFileItems);
+    };
+    uploader.onBeforeUploadItem = function(item) {
+      console.info('onBeforeUploadItem', item);
+    };
+    uploader.onProgressItem = function(fileItem, progress) {
+      console.info('onProgressItem', fileItem, progress);
+    };
+    uploader.onProgressAll = function(progress) {
+      console.info('onProgressAll', progress);
+    };
+    uploader.onSuccessItem = function(fileItem, response, status, headers) {
+      console.info('onSuccessItem', fileItem, response, status, headers);
+    };
+    uploader.onErrorItem = function(fileItem, response, status, headers) {
+      console.info('onErrorItem', fileItem, response, status, headers);
+    };
+    uploader.onCancelItem = function(fileItem, response, status, headers) {
+      console.info('onCancelItem', fileItem, response, status, headers);
+    };
+    uploader.onCompleteItem = function(fileItem, response, status, headers) {
+      console.info('onCompleteItem', fileItem, response, status, headers);
+    };
+    uploader.onCompleteAll = function() {
+      console.info('onCompleteAll');
     };
 
-    $scope.cancel = function () {
-      $modalInstance.dismiss('cancel');
+    console.info('uploader', uploader);
+  }]);
+
+  // Thumbnail directive
+  angular.module('mean.system').directive('ngThumb', ['$window', function($window) {
+    var helper = {
+      support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+      isFile: function(item) {
+        return angular.isObject(item) && item instanceof $window.File;
+      },
+      isImage: function(file) {
+        var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+        return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+      }
     };
 
-    $scope.flowOpts = {
-      target: '/api/letters' // /api/mailStops/1234567890
+    return {
+      restrict: 'A',
+      template: '<canvas/>',
+      link: function(scope, element, attributes) {
+        if (!helper.support) return;
+
+        var params = scope.$eval(attributes.ngThumb);
+
+        if (!helper.isFile(params.file)) return;
+        if (!helper.isImage(params.file)) return;
+
+        var canvas = element.find('canvas');
+        var reader = new FileReader();
+
+        reader.onload = onLoadFile;
+        reader.readAsDataURL(params.file);
+
+        function onLoadFile(event) {
+          var img = new Image();
+          img.onload = onLoadImage;
+          img.src = event.target.result;
+        }
+
+        function onLoadImage() {
+          var width = params.width || this.width / this.height * params.height;
+          var height = params.height || this.height / this.width * params.width;
+          canvas.attr({ width: width, height: height });
+          canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+        }
+      }
     };
-}]);
+  }
+]);
