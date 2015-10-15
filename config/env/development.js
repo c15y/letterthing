db0ce2f2f0d0ac4a0752e35249271511e5952cf0
Blue <blue@ismashrobots.com>
2015-10-15 00:49:15 -0700

    email env

--- config/env/development.js
+++ config/env/development.js
@@ -52,5 +52,13 @@ module.exports = {
       enabled: false
     }
   },
+  emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
+  mailer: {
+    service: 'SERVICE_PROVIDER', // Gmail, SMTP
+    auth: {
+      user: 'EMAIL_ID',
+      pass: 'PASSWORD'
+    }
+  },
   secret: 'SOME_TOKEN_SECRET'
 };
