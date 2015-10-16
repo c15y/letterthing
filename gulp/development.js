c21b20d68c7023f8957432b92bc2896372fed9d0
Blue <blue@ismashrobots.com>
2015-10-15 22:31:15 -0700

    fixed broken livereload

--- gulp/development.js
+++ gulp/development.js
@@ -66,7 +66,7 @@ gulp.task('devServe', ['env:development'], function () {
     stdout: false
   }).on('readable', function() {
     this.stdout.on('data', function(chunk) {
-      if(/Mean app started/.test(chunk)) {
+      if(/Letter Thing started/.test(chunk)) {
         setTimeout(function() { plugins.livereload.reload(); }, 500);
       }
       process.stdout.write(chunk);
