b010ee075df40bd31d01e1a3648e89b5403aafe1
Blue <blue@ismashrobots.com>
2015-10-17 02:39:07 -0700

    remodeling

--- packages/core/system/server/models/mailboxes.js
+++ packages/core/system/server/models/mailboxes.js
@@ -2,27 +2,76 @@
 
 var mongoose  = require('mongoose'),
      Schema   = mongoose.Schema,
-          _   = require('lodash'),
-   modelUtils = require('./model-utils');
+   PhoneRegEx = /^[2-9][0-9]{0,9}$/,
+   StateRegEx = /^((A[LKZR])|(C[AOT])|(D[EC])|(FL)|(GA)|(HI)|(I[DLNA])|(K[SY])|(LA)|(M[EDAINSOT])|(N[EVHJMYCD])|(O[HKR])|(PA)|(RI)|(S[CD])|(T[NX])|(UT)|(V[TA])|(W[AVIY]))$/,
+     ZipRegEx = /^[0-9]{5}(?:-[0-9]{4})?$/,
+PasscodeRegEx = /^[A-Z]+$/;
 
 var MailboxSchema = new Schema({
   _id: {
     type: String,
     unique: true,
-    match: modelUtils.PhoneRegEx
+    match: PhoneRegEx,
+    required: true
   },
-  name: { type: String, required: false },
-  letters: [ mongoose.model('Letter').schema ],
+  name: { type: String },
+  address: {
+    street1: { type: String, uppercase: true, trim: true, required: true },
+    street1: { type: String, uppercase: true, trim: true, },
+    city: { type: String, uppercase: true, trim: true, required: true},
+    state: { type: String, uppercase: true, trim: true, match: StateRegEx, required: true },
+    zip: { type: String, match: ZipRegEx }
+  },
+  letters: [{
+    operator: { type: String, match: PhoneRegEx, required: true },
+    direction: { type: String, enum: ['incoming', 'outgoing'], required: true },
+    created: { type: Date, required: true },
+    mailed: Date,
+    address: {
+      street1: { type: String, uppercase: true, trim: true, required: true },
+      street1: { type: String, uppercase: true, trim: true, },
+      city: { type: String, uppercase: true, trim: true, required: true},
+      state: { type: String, uppercase: true, trim: true, match: StateRegEx, required: true },
+      zip: { type: String, match: ZipRegEx, required: true }
+    },
+    passcode: { type: String, uppercase: true, match: PasscodeRegEx },
+    infoPages: {
+      type: [{
+        image: {
+          data: Buffer,
+          contentType: String
+        }
+      }],
+      private: true
+    },
+    publicPages: [{
+      image: {
+        data: Buffer,
+        contentType: String
+      }
+    }],
+    paymentPages: {
+      type: [{
+        image: {
+          data: Buffer,
+          contentType: String
+        },
+        amount: Number,
+        type: { type: String, enum: ['checks', 'stripe'] },
+        ref: { type: String, required: false },
+        cleared: { type: Date, required: false }
+      }],
+      private: true
+    },
+    cost: Number
+  }],
   operator: {
     type: {
-      user: { type: Schema.Types.ObjectId, unique: true },
-      office: { type: String, match: modelUtils.PhoneRegEx }
+      user: { type: Schema.Types.ObjectId, unique: true, required: true },
+      office: { type: String, match: PhoneRegEx, required: true }
     },
-    required: false,
     sparse: true
   }
 });
-
-modelUtils.allFieldsRequiredByDefautlt(MailboxSchema);
 
 exports.Mailbox = mongoose.model('Mailbox', MailboxSchema);
--- packages/core/system/server/routes/mailboxes.js
+++ packages/core/system/server/routes/mailboxes.js
@@ -2,14 +2,8 @@
 
 var mongoose = require('mongoose'),
 Mailbox = mongoose.model('Mailbox'),
-Letter = mongoose.model('Letter'),
-_ = require('lodash'),
 restify = require('express-restify-mongoose');
 
 module.exports = function(System, app, auth, database) {
-
   restify.serve(app, Mailbox);
-
-  restify.serve(app, Letter);
-
 };
