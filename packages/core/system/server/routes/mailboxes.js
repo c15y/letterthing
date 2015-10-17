cf89ec19c0b287415095c8cf6eaf3cb557011f6d
Blue <blue@ismashrobots.com>
2015-10-16 21:30:54 -0700

    cleanup

--- packages/core/system/server/routes/mailboxes.js
+++ packages/core/system/server/routes/mailboxes.js
@@ -9,17 +9,6 @@ var mongoose = require('mongoose'),
 module.exports = function(System, app, auth, database) {
     var mailboxes = require('../controllers/mailboxes');
 
-    app.param('code', mailboxes.mailbox);
-
-    // app.get('/api/mailboxes', mailboxes.all);
-    app.post('/api/mailboxes', auth.requiresAdmin, mailboxes.create);
-
-    app.get('/api/mailboxes/:code', mailboxes.get);
-    //app.get('/api/mailboxes/:code', mailboxes.get);
-
-    // app.put('/api/mailboxes/:code', auth.requiresAdmin, mailboxes.update);
-    // app.delete('/api/mailboxes/:code', auth.requiresAdmin, mailboxes.delete);
-
     restify.serve(app, Mailbox);
 
     restify.serve(app, Letter);
