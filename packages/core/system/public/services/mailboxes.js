9058759faa68a19e6aa72fc11bec64747cb78b65
Blue <blue@ismashrobots.com>
2015-10-19 00:44:57 -0700

    code -> phone

--- packages/core/system/public/services/mailboxes.js
+++ packages/core/system/public/services/mailboxes.js
@@ -2,8 +2,8 @@
 
 angular.module('mean.system').factory('Mailboxes', ['$resource',
   function($resource) {
-    return $resource('/api/v1/mailboxes/:code', {
-      code: '@code'
+    return $resource('/api/v1/mailboxes/:mailbox', {
+      mailbox: '@mailbox'
     }, {
       get: {
         method: 'GET',
